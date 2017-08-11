7c466b60e5aa79a87029472609801aa2bac28e37
indraaguslesmana <indra953@gmail.com>
2017-08-11 13:47:07 +0700

    make fullscreen video if, "phone" rotate

--- app/src/main/java/id/co/blogspot/tutor93/bakingapps/main_detail/MainDetailFragment.java
+++ app/src/main/java/id/co/blogspot/tutor93/bakingapps/main_detail/MainDetailFragment.java
@@ -1,6 +1,7 @@
 package id.co.blogspot.tutor93.bakingapps.main_detail;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Handler;
 import android.support.design.widget.CollapsingToolbarLayout;
@@ -176,7 +177,27 @@ public class MainDetailFragment extends Fragment {
             }
         });
 
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE &&
+                !getResources().getBoolean(R.bool.isTablet)){
+            hideSystemUI();
+            exoPlayerView.getLayoutParams().height = ViewGroup.LayoutParams.MATCH_PARENT;
+            exoPlayerView.getLayoutParams().width = ViewGroup.LayoutParams.MATCH_PARENT;
+        }
+
         return rootView;
+    }
+
+    private void hideSystemUI() {
+        // Set the IMMERSIVE flag.
+        // Set the content to appear under the system bars so that the content
+        // doesn't resize when the system bars hide and show.
+        mActivity.getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+                        | View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 
     private MediaSource buildMediaSource(Uri uri, String overrideExtension) {
