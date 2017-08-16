28f9243222d9ed95b1c56f6f01398eb53ef2e3e5
indraaguslesmana <indra953@gmail.com>
2017-08-16 09:30:34 +0700

    make player can play video.

--- app/src/main/java/id/co/blogspot/tutor93/bakingapps/main_detail/ItemDetailFragment.java
+++ app/src/main/java/id/co/blogspot/tutor93/bakingapps/main_detail/ItemDetailFragment.java
@@ -3,16 +3,49 @@ package id.co.blogspot.tutor93.bakingapps.main_detail;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.exoplayer2.C;
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.source.hls.HlsMediaSource;
+import com.google.android.exoplayer2.source.smoothstreaming.DefaultSsChunkSource;
+import com.google.android.exoplayer2.source.smoothstreaming.SsMediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.upstream.HttpDataSource;
+import com.google.android.exoplayer2.util.Util;
 
 import id.co.blogspot.tutor93.bakingapps.R;
 import id.co.blogspot.tutor93.bakingapps.data.network.response.BakingResponse;
@@ -42,6 +75,12 @@ public class ItemDetailFragment extends Fragment {
      */
     private AppCompatActivity mActivity;
     private SimpleExoPlayerView exoPlayerView;
+    private SimpleExoPlayer player;
+    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+    private DataSource.Factory mediaDataSourceFactory;
+    private Handler mainHandler;
+    private static final String TAG = "ItemDetailFragment";
+
 
     public ItemDetailFragment() {
     }
@@ -63,7 +102,7 @@ public class ItemDetailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.activity_item_detail, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_item_detail, container, false);
 
         initView(rootView);
         checkOrientation(mActivity);
@@ -75,6 +114,55 @@ public class ItemDetailFragment extends Fragment {
         mActivity = (AppCompatActivity) getActivity();
 
         exoPlayerView = (SimpleExoPlayerView) rootview.findViewById(R.id.exoPlayerView);
+        mediaDataSourceFactory = buildDataSourceFactory(true);
+
+        mainHandler = new Handler();
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTrackSelectionFactory =
+                new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTrackSelectionFactory);
+
+        LoadControl loadControl = new DefaultLoadControl();
+
+        player = ExoPlayerFactory.newSimpleInstance(mActivity, trackSelector, loadControl);
+        exoPlayerView = (SimpleExoPlayerView) rootview.findViewById(R.id.exoPlayerView);
+        exoPlayerView.setPlayer(player);
+        exoPlayerView.setUseController(true);
+        exoPlayerView.requestFocus();
+
+        if (!mItem.steps.get(0).videoURL.isEmpty()){
+            String videoUrl = mItem.steps.get(0).videoURL;
+            Uri uri = Uri.parse(videoUrl);
+            MediaSource mediaSource = buildMediaSource(uri, "mp3");
+            player.prepare(mediaSource);
+        }
+
+        player.addListener(new ExoPlayer.EventListener() {
+            @Override public void onTimelineChanged(Timeline timeline, Object manifest) {
+                Log.d(TAG, "onTimelineChanged: ");
+            }
+
+            @Override public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+                Log.d(TAG, "onTracksChanged: " + trackGroups.length);
+            }
+
+            @Override public void onLoadingChanged(boolean isLoading) {
+                Log.d(TAG, "onLoadingChanged: " + isLoading);
+            }
+
+            @Override public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+                Log.d(TAG, "onPlayerStateChanged: " + playWhenReady);
+            }
+
+            @Override public void onPlayerError(ExoPlaybackException error) {
+                Log.e(TAG, "onPlayerError: ", error);
+            }
+
+            @Override public void onPositionDiscontinuity() {
+                Log.d(TAG, "onPositionDiscontinuity: true");
+            }
+        });
+
     }
 
     private void checkOrientation(Activity context) {
@@ -99,4 +187,37 @@ public class ItemDetailFragment extends Fragment {
                         | View.SYSTEM_UI_FLAG_IMMERSIVE);
     }
 
+    private MediaSource buildMediaSource(Uri uri, String overrideExtension) {
+        int type = TextUtils.isEmpty(overrideExtension) ? Util.inferContentType(uri)
+                : Util.inferContentType("." + overrideExtension);
+        switch (type) {
+            case C.TYPE_SS:
+                return new SsMediaSource(uri, buildDataSourceFactory(false),
+                        new DefaultSsChunkSource.Factory(mediaDataSourceFactory), mainHandler, null);
+            case C.TYPE_DASH:
+                return new DashMediaSource(uri, buildDataSourceFactory(false),
+                        new DefaultDashChunkSource.Factory(mediaDataSourceFactory), mainHandler, null);
+            case C.TYPE_HLS:
+                return new HlsMediaSource(uri, mediaDataSourceFactory, mainHandler, null);
+            case C.TYPE_OTHER:
+                return new ExtractorMediaSource(uri, mediaDataSourceFactory, new DefaultExtractorsFactory(),
+                        mainHandler, null);
+            default: {
+                throw new IllegalStateException("Unsupported type: " + type);
+            }
+        }
+    }
+
+    private DataSource.Factory buildDataSourceFactory(boolean useBandwidthMeter) {
+        return buildDataSourceFactory(useBandwidthMeter ? BANDWIDTH_METER : null);
+    }
+
+    public DataSource.Factory buildDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
+        return new DefaultDataSourceFactory(mActivity, bandwidthMeter,
+                buildHttpDataSourceFactory(bandwidthMeter));
+    }
+
+    public HttpDataSource.Factory buildHttpDataSourceFactory(DefaultBandwidthMeter bandwidthMeter) {
+        return new DefaultHttpDataSourceFactory(Util.getUserAgent(mActivity, "ExoPlayerDemo"), bandwidthMeter);
+    }
 }
