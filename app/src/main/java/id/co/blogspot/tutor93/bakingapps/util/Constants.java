e1a99cd86750e4f4b86bf4955f11b00059570ca8
indraaguslesmana <indra953@gmail.com>
2017-08-27 08:17:45 +0700

    fix orientation state on detail list [phone].

--- app/src/main/java/id/co/blogspot/tutor93/bakingapps/util/Constants.java
+++ app/src/main/java/id/co/blogspot/tutor93/bakingapps/util/Constants.java
@@ -4,6 +4,6 @@ package id.co.blogspot.tutor93.bakingapps.util;
  * Created by indraaguslesmana on 8/6/17.
  */
 
-public class Constants {
+public final class Constants {
     public static boolean ENABLE_LOG = true;
 }
